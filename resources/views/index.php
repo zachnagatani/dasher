559bda51e7880d7a0c0f5d8197cd079713ceb187
Zachariah Nagatani <zachnagatani@gmail.com>
2017-05-02 16:59:55 -0700

    Ember/Laravel Integration

---
+++ resources/views/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Dasher</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+<meta name="dasher/config/environment" content="%7B%22modulePrefix%22%3A%22dasher%22%2C%22environment%22%3A%22development%22%2C%22rootURL%22%3A%22/%22%2C%22locationType%22%3A%22auto%22%2C%22EmberENV%22%3A%7B%22FEATURES%22%3A%7B%7D%2C%22EXTEND_PROTOTYPES%22%3A%7B%22Date%22%3Afalse%7D%7D%2C%22APP%22%3A%7B%22name%22%3A%22dasher%22%2C%22version%22%3A%220.0.0+0bc65429%22%7D%2C%22exportApplicationGlobal%22%3Atrue%7D" />
+
+    <link rel="stylesheet" href="/dasher/dist/assets/vendor.css">
+    <link rel="stylesheet" href="/dasher/dist/assets/dasher.css">
+
+
+  </head>
+  <body>
+
+
+    <script src="/dasher/dist/assets/vendor.js"></script>
+    <script src="/dasher/dist/assets/dasher.js"></script>
+
+
+  </body>
+</html>
